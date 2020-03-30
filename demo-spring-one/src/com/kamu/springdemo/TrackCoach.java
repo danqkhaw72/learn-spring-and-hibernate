905c9ae89aa03216b75e3b7f436db2357763e4ab
Dang Kham <danqkhaw72@gmail.com>
2020-03-30 15:59:38 +0700

    Add Spring Bean Scopes and Lifecycle methods

--- demo-spring-one/src/com/kamu/springdemo/TrackCoach.java
+++ demo-spring-one/src/com/kamu/springdemo/TrackCoach.java
@@ -29,5 +29,15 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just do it: " + FortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyClearStartupStuff() {
+		System.out.println("TrackCoach: inside method doClearStartupStuff");
+	}
 
 }
