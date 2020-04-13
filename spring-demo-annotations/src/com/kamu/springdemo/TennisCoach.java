bb3caffc9f4d3b11c6341305e2f5ead88bbb8bea
Dang Kham <danqkhaw72@gmail.com>
2020-04-13 08:05:00 +0700

    Bean Scope

--- spring-demo-annotations/src/com/kamu/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/kamu/springdemo/TennisCoach.java
@@ -1,5 +1,8 @@
 package com.kamu.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -14,6 +17,18 @@ public class TennisCoach implements Coach {
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println("TennisCoach: inside of doMyStartupCoach()");
+	}
+	
+	// difine my destroy method
+	@PreDestroy
+	public void doMyClearupStuff() {
+		System.out.println("TennisCoach: inside of doMyClearupStuff()");
 	}
 	
 	/*
